7807bf5625a653211d482dd1f52f5e8a08f30ba7
Dejan <dejan.orter@outlook.com>
2018-05-10 13:15:02 +0200

    test

--- featureSelection.py
+++ featureSelection.py
@@ -1,4 +1,4 @@
 #Feature selection, also called variable selection is a data reduction technique for selecting 
 # the most relevant features of a learning models. As irrelevant and redundant features are 
-# removed the model’s accuracy increases.
-#test
+# removed the models accuracy increases
+
