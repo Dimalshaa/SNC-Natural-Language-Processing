627297090c6a5df35e8c368062fee361b250185d
unknown <gregor17.kovacevic@gmail.com>
2018-05-11 17:20:09 +0200

    Dodano pred-procesiranje besedila (pisem)

---
+++ preProcessing.py
@@ -0,0 +1,60 @@
+# -*- coding: cp1252 -*-
+import os
+import re
+
+def zgradiListoDatotek(path):
+    datoteke = os.listdir(path)
+
+    for i in range(0, len(datoteke)):
+        print("Datoteka: " + datoteke[i])
+
+
+    return datoteke
+
+def preberiDatoteko(dat):
+    with file(dat) as f:
+        string = f.read()
+
+    return string
+
+
+#popraviBesedilo -> odstrani locila, stevilke in vsa druga locila,
+#spremeni velike crke v male.
+def popraviBesedilo(besedilo):
+    izhod = ""
+    
+    for char in besedilo:
+        if char.isalpha():
+            if char.isupper():
+                izhod+=char.lower()
+            else:    
+                izhod+=char
+        elif char.isspace():
+            izhod+=char
+        elif char=="’":
+            izhod+=char
+        
+    return izhod
+
+def shraniDatoteko(path, filename, besedilo):
+    dat = path + "/" + filename   
+    file_out  = open(dat, 'w')
+
+    file_out.write(besedilo)
+
+     
+    
+if __name__ == "__main__":
+    #path -> mapa kjer se nahajajo datoteke za obdelavo
+    #path_izhod -> mapa v katero bomo shranjevali popravljene datoteke
+    path = "./suicide-notes-database/raw/genuine"
+    path_izhod = "./suicide-notes-database/raw/genuine-predobdelana"
+    
+    datoteke = zgradiListoDatotek(path)
+
+    for i in range(0, len(datoteke)):
+        filename = path + '/' + datoteke[i]
+        besedilo = preberiDatoteko(filename)
+        popravljeno_besedilo = popraviBesedilo(besedilo)
+        shraniDatoteko(path_izhod, datoteke[i], popravljeno_besedilo)
+       
