6b400c8687f2489f61aa8678b847178bc707e65e
Dejan Rupnik <dejan.rupnik.1@gmail.com>
2018-05-12 18:50:17 +0200

    dodana podpora za izpis statistike

--- partsOfSpeech.py
+++ partsOfSpeech.py
@@ -8,13 +8,18 @@ import nltk
 from multiprocessing import Process, Manager, Value
 import math
 import json
+import pickle
+import codecs
+import uuid
+import ast
+#from nltk.corpus import brown
 
 """
 outputs file contents as string
 """
 def textFileParser(filename):
     data = []
-    with open(filename, 'r') as text_file:
+    with codecs.open(filename, 'r', encoding='utf-8', errors='ignore') as text_file:
         data=text_file.read().replace('\n', '')
     return [data]
 
@@ -61,6 +66,7 @@ downloads all lexicons and other files, used by nltk
 def downloadLexicons(): 
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
+    nltk.download('brown')
 
 class PartsOfSpeechTagger:
     def __init__(self, results_folder_path):
@@ -77,16 +83,13 @@ class PartsOfSpeechTagger:
     ### multi thread methods
     def tagger(self, data, processID):
         print("[PartsOfSpeechTagger] Process initialized")
-        output = []
         for paragraph in data:
             tokenized = nltk.word_tokenize(paragraph.decode('utf-8'))
-            output.append(nltk.pos_tag(tokenized))
-            
-        json_string = json.dumps(output, ensure_ascii=False) 
-        filename = self.results_folder_path + "test" + str(processID)  
-        with open(filename, 'w') as outfile:
-            json.dump(json_string, outfile) 
-
+            json_string = json.dumps(nltk.pos_tag(tokenized), ensure_ascii=False) 
+            filename = self.results_folder_path + str(uuid.uuid1()) +".json"
+            with open(filename, 'w') as outfile:
+                json.dump(json_string, outfile) 
+        
         return True
 
     def partsOfSpeechTaggingMultiprocessed(self, data, number_of_processes):
@@ -120,16 +123,95 @@ class PartsOfSpeechTagger:
         for k in range(0, len(processes)):
             processes[k].join() 
 
+class PartsOfSpeechReader():
+    def __init__(self, pos_folder_path, statistics_folder_path):
+        self.pos_folder_path = pos_folder_path
+        self.statistics_folder_path = statistics_folder_path
+
+    # get data as an array
+    def readAnalyzedData(self):
+        filenames = os.listdir(self.pos_folder_path)
+        output = []
+        for i in range(0, len(filenames)):
+            with open(self.pos_folder_path + filenames[i]) as json_data_encrypted:
+                note = json.load(json_data_encrypted)
+                output.append(note)
+        return output
+ 
+    # save to file
+    def save_obj(self, obj):
+        with open(self.statistics_folder_path + str(uuid.uuid1()) + '.pkl', 'wb') as f:
+            pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+    # save dictionaries of selected notes into a statistics files folder
+    def partsOfSpeechStatistics(self, pos_data):
+        notes = pos_data
+        for note in notes:
+            note = ast.literal_eval(note)
+            dict = {}
+            for tagged_word in note:
+                if tagged_word[1] in dict:
+                    dict[tagged_word[1]] += 1.0
+                else:
+                    dict[tagged_word[1]] = 1.0
+
+            self.save_obj(dict)
+
+            """
+            for tag in dict:
+                print tag, ' : ', dict[tag]
+            """
+        return
+
+    # load from file
+    def load_obj(self):
+        filenames = os.listdir(self.statistics_folder_path)
+        output = []
+        for i in range(0, len(filenames)):
+            with open(self.statistics_folder_path + filenames[i], 'rb') as f:
+                output.append(pickle.load(f))
+        return output
+
+    # gets median values
+    def getMedianStatistics(self):
+        dictionaries = self.load_obj()
+        results = {}
+        for d in dictionaries:
+            for key in d:
+                if key in results:
+                    results[key] += d[key]
+                else:
+                    results[key] = d[key]
+        
+        for key in results:
+            #print key, " : ", results[key]
+            results[key] /= len(dictionaries)
+            #print key, " : ", results[key]
+        
+        return results
+
 
 
 if __name__ == "__main__":
-   # downloadLexicons()
-    data = readFiles("./suicide-notes-database/trash_files", ".//post")
-    
-    pos = PartsOfSpeechTagger("./suicide-notes-database/results/")
+    #downloadLexicons()
+    data = readFiles("./suicide-notes-database/completers/", ".//post")
+    pos = PartsOfSpeechTagger("./suicide-notes-database/parts_of_speech/")
+
+    # single threaded tagger
     #pos.partsOfSpeechTagging(data)
     #print(len(pos.data_tagged))
 
-    #multithreaded
-    pos.partsOfSpeechTaggingMultiprocessed(data, 6)
-    
+    # multi threaded tagger
+    #pos.partsOfSpeechTaggingMultiprocessed(data, 6)
+
+    # reader : statistics
+    pos_reader = PartsOfSpeechReader(
+        "./suicide-notes-database/parts_of_speech/",
+        "./suicide-notes-database/parts_of_speech_statistics/"
+        )
+    #pos_data = pos_reader.readAnalyzedData()
+    #pos_reader.partsOfSpeechStatistics(pos_data)
+    statistics = pos_reader.getMedianStatistics()
+
+
+
