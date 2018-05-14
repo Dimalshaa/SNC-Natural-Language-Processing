9c5af16aab5653b91bfe7a592e21ff9db541a0c6
Dejan Rupnik <dejan.rupnik.1@gmail.com>
2018-05-15 01:47:20 +0200

    urejen clasifikator, za boljso preglednost kode

--- main_classificator.py
+++ main_classificator.py
@@ -25,52 +25,40 @@ def fleschMediana(path):
 
     return meanFLESCH, meanKINCAID
 
-
-if __name__ == "__main__":
-    #downloadLexicons()
+"""
+saves CVS file of means for completers and elicitors
+"""
+def saveMeanStatisticsCSV(output_csv_filename, pos_completers_folder, pos_elicitors_folder, pos_folder_path, statistics_folder_path, 
+    paragraph_tag_name, readibility_completers_folder, readibility_elicitors_folder):
     
     csv_attributes = ["Attribute", "Completers Mean (SD)", "Elicitors Mean (SD)"]
     #cvs_mean_pos = [[key , completer_mean, elicitor_mean], ...]
     cvs_mean_pos = []
 
-       
-    #### LEARNING SET
-    ### COMPLETERS
-    data = readFiles("./suicide-notes-database/completers-pp/", ".//post")
-    pos = PartsOfSpeechTagger("./suicide-notes-database/parts_of_speech/")
-
-    # multi threaded tagger
-    pos.partsOfSpeechTaggingMultiprocessed(data, 6)
-
-    
-    # reader : statistics
+    ### POS statistics
+    pos = PartsOfSpeechTagger(pos_folder_path)
     pos_reader = PartsOfSpeechReader(
-        "./suicide-notes-database/parts_of_speech/",
-        "./suicide-notes-database/parts_of_speech_statistics/"
+        pos_folder_path,
+        statistics_folder_path
         )
+  
+    ### COMPLETERS
+    data = readFiles(pos_completers_folder, paragraph_tag_name)
+    pos.partsOfSpeechTaggingMultiprocessed(data, 6)
     pos_reader.partsOfSpeechStatistics( pos_reader.readAnalyzedData() )
     statistics_completers = pos_reader.getMedianStatistics()
 
     ### ELICITORS
-    data = readFiles("./suicide-notes-database/elicitors-pp/", ".//post")
-    pos = PartsOfSpeechTagger("./suicide-notes-database/parts_of_speech/")
-
-    # multi threaded tagger
+    data = readFiles(pos_elicitors_folder, paragraph_tag_name)
     pos.partsOfSpeechTaggingMultiprocessed(data, 6)
-
-    # reader : statistics
-    pos_reader = PartsOfSpeechReader(
-        "./suicide-notes-database/parts_of_speech/",
-        "./suicide-notes-database/parts_of_speech_statistics/"
-        )
     pos_reader.partsOfSpeechStatistics( pos_reader.readAnalyzedData() )
     statistics_elicitors = pos_reader.getMedianStatistics()
     
-    ###Flesch-ova metoda in metoda Flesch-Kincaid -> COMPLETERS
-    flesch_completers, kincaid_completers = fleschMediana("./suicide-notes-database/completers/")
+    ### Flesch-ova metoda in metoda Flesch-Kincaid -> COMPLETERS
+    flesch_completers, kincaid_completers = fleschMediana(readibility_completers_folder)
 
-    ###Flesch-ova metoda in metoda Flesch-Kincaid -> ELICITORS
-    flesch_elicitors, kincaid_elicitors = fleschMediana("./suicide-notes-database/elicitors/")
+    ### Flesch-ova metoda in metoda Flesch-Kincaid -> ELICITORS
+    flesch_elicitors, kincaid_elicitors = fleschMediana(readibility_elicitors_folder)
 
     cvs_mean_pos.append(["Flesch score", flesch_completers, flesch_elicitors])
     cvs_mean_pos.append(["Flesch-Kincaid grade", kincaid_completers, kincaid_elicitors])
@@ -78,10 +66,18 @@ if __name__ == "__main__":
     for key in statistics_elicitors:
         cvs_mean_pos.append([key, statistics_completers[key], statistics_elicitors[key]])
 
-
-    with open("medians.csv", 'wb') as fs:
+    with open(output_csv_filename, 'wb') as fs:
         wr = csv.writer(fs, quoting=csv.QUOTE_NONE) #, delimiter='|', quotechar='',escapechar='\\')
         wr.writerow(csv_attributes)
         for tag in cvs_mean_pos:
             wr.writerow(tag)
+
+
+
+if __name__ == "__main__":
+    #downloadLexicons()
+    
+    saveMeanStatisticsCSV("means.csv", "./suicide-notes-database/completers-pp/", "./suicide-notes-database/elicitors-pp/",
+        "./suicide-notes-database/parts_of_speech/", "./suicide-notes-database/parts_of_speech_statistics/", ".//post",
+        "./suicide-notes-database/completers/", "./suicide-notes-database/elicitors/")
 
