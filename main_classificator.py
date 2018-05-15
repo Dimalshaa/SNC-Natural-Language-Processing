79ada1abdfc698f8a5fc983868896a87d7c74ca5
unknown <gregor17.kovacevic@gmail.com>
2018-05-15 20:17:36 +0200

    Dodana statistika nad posameznim pismom

--- main_classificator.py
+++ main_classificator.py
@@ -25,6 +25,73 @@ def fleschMediana(path):
 
     return meanFLESCH, meanKINCAID
 
+def singleFileStatisticsCSV(completers_folder_pp, elicitors_folder_pp, statistics_folder, pos_folder, out_completers_filename,
+                            out_elictors_filename, regular_completers_folder, regular_elicitors_folder):
+
+    full_data_completers = []
+    full_data_elicitors = []
+    
+    paragraph_tag_name = ".//post"
+    full_data_completers = getFileByFileStatistics(completers_folder_pp, pos_folder, statistics_folder, paragraph_tag_name)
+    full_data_elicitors = getFileByFileStatistics(elicitors_folder_pp, pos_folder, statistics_folder, paragraph_tag_name)
+
+    num_completers = len(full_data_completers)
+    num_elicitors = len(full_data_elicitors)
+
+    csv_out_completers = []
+    csv_out_elicitors = []
+
+    results=rt.readibility_test(regular_completers_folder, "*.txt")
+    results_2=rt.readibility_test(regular_elicitors_folder, "*.txt") 
+
+    for i in range(0, num_completers):
+        output = []
+        path = full_data_completers[i][0][0]
+        words = path.split("/")
+        filename = words[-1]                # -1 = zadni element
+        
+        output.extend(filename)
+        
+        dictionary = full_data_completers[i][0][1][0]
+        for key in dictionary
+            output.extend(dictionary[key]) 
+
+        res = results[i];
+        output.extend(float(res[1]));
+        output.extend(float(res[2]));
+        csv_out_completers.append(output)
+
+    with open(out_completers_filename, 'wb') as fs:
+        wr = csv.writer(fs, quoting=csv.QUOTE_NONE)
+        wr.writerow(csv_attributes)
+        for tag in cvs_mean_pos:
+           wr.writerow(tag)
+
+    for i in range(0, num_elicitors):
+        output_2 = []
+        path_2 = full_data_elicitors[i][0][0]
+        words_2 = path_2.split("/")
+        filename_2 = words_2[-1]                # -1 = zadni element
+        
+        output_2.extend(filename_2)
+        
+        dictionary_2 = full_data_elicitors[i][0][1][0]
+        for key in dictionary_2
+            output_2.extend(dictionary_2[key])
+
+        res_2 = results_2[i];
+        output.extend(float(res_2[1]));
+        output.extend(float(res_2[2]));
+        csv_out_elicitors.append(output)
+
+
+    with open(out_elictors_filename, 'wb') as fs:
+        wr = csv.writer(fs, quoting=csv.QUOTE_NONE)
+        wr.writerow(csv_attributes)
+        for tag in cvs_mean_pos:
+           wr.writerow(tag)        
+   
+
 """
 saves CVS file of means for completers and elicitors
 """
@@ -80,4 +147,10 @@ if __name__ == "__main__":
     saveMeanStatisticsCSV("means.csv", "./suicide-notes-database/completers-pp/", "./suicide-notes-database/elicitors-pp/",
         "./suicide-notes-database/parts_of_speech/", "./suicide-notes-database/parts_of_speech_statistics/", ".//post",
         "./suicide-notes-database/completers/", "./suicide-notes-database/elicitors/")
+
+    singleFileStatisticsCSV("./suicide-notes-database/completers-pp/", "./suicide-notes-database/elicitors-pp/",
+                            "./suicide-notes-database/parts_of_speech_statistics/", "./suicide-notes-database/parts_of_speech/",
+                            "./single-letter-statistics-completers/", "./single-letter-statistics-elicitos/")
+                            
+                            
 
