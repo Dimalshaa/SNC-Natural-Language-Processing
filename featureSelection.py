93ada08e9fc215b8eb9484c0f885964a60b75304
Dejan <dejan.orter@outlook.com>
2018-05-16 18:08:06 +0200

    Splošna statistika imeDatoteke1.txt|st.besed|st.znakov|st.crk|st.stavkov imeDatoteke2.txt|st.besed|st.znakov|st.crk|st.stavkov imeDatoteke3.txt|st.besed|st.znakov|st.crk|st.stavkov ...

--- featureSelection.py
+++ featureSelection.py
@@ -2,7 +2,9 @@
 # the most relevant features of a learning models. As irrelevant and redundant features are
 
 import os
+import re
 import time
+import numpy
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,6 +13,56 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 
 
+def prestejStavke(datoteka):
+    stStavkov = 0
+    stavki = datoteka.split(".")
+    stStavkov = stStavkov + len(stavki) -1
+    stavki = datoteka.split("!")
+    stStavkov = stStavkov + len(stavki) -1
+    stavki = datoteka.split("?")
+    stStavkov = stStavkov + len(stavki) -1
+    #print(stStavkov)
+    if(stStavkov == 0): # ker ne zazna ...
+        return 1
+    else:
+        return stStavkov
+
+def prestejBesede(datoteka):
+   # print(len(datoteka.split()))
+    return len(datoteka.split())
+
+def prestejZnake(datoteka):
+    dolzinaDatoteke = len(datoteka)
+    samoCrke = re.sub('[^0-9a-zA-Z]+', '', datoteka)
+    #print("Samo crke: " + samoCrke)
+    stZnakov = dolzinaDatoteke - len(samoCrke)
+    #print("Stevilo znakov: {0}".format(stZnakov))
+    return stZnakov
+
+def prestejCrke(datoteka):
+    samoCrke = re.sub('[^0-9a-zA-Z]+', '', datoteka)
+    #print("Stevilo crk: {0}".format(len(samoCrke)))
+    return len(samoCrke)
+
+def splosnaStatistika(path):
+    files = []
+    statistika = []
+    if isinstance(path, (list,)):
+        for j in range(0, len(path)):
+            files.extend(buildFileList(path[j]))
+            for i in range(0, len(files)):
+                filename = path[j] + '/' + files[i]
+                content = readFile(filename)
+                statistika.append("{0}|{1}|{2}|{3}|{4}|".format(files[i], prestejBesede(content), prestejZnake(content),  prestejCrke(content),  prestejStavke(content)))
+            files = []
+    else:
+        files.extend(buildFileList(path))
+        for i in range(0, len(files)):
+            filename = path + '/' + files[i]
+            content = readFile(filename)
+            statistika.append("{0}|{1}|{2}|{3}|{4}|".format(files[i], prestejBesede(content), prestejZnake(content),prestejCrke(content), prestejStavke(content)))
+    return statistika
+
 def bagOfWords(corpus):
     vectorizer = CountVectorizer()
     print(vectorizer.fit_transform(corpus).todense())
@@ -18,18 +70,15 @@ def bagOfWords(corpus):
 
 
 def LSA(corpus):
-    vectorizer = TfidfVectorizer(max_df=0.75, max_features=10000, min_df=1, use_idf=True)
+    vectorizer = TfidfVectorizer(max_df=1.0, max_features=10000, min_df=0.1, use_idf=True)
     tfidf = vectorizer.fit_transform(corpus)
-
-    print(tfidf.todense())
     print(vectorizer.vocabulary_)
-
-    print("  Actual number of tfidf features: %d" % tfidf.get_shape()[1])
-    svd = TruncatedSVD(93)
+    feat_names = vectorizer.get_feature_names()
+    print(feat_names)
+    svd = TruncatedSVD(10)
     lsa = make_pipeline(svd, Normalizer(copy=False))
-    explained_variance = svd.explained_variance_ratio_.sum()
-    print("SVD: {}%".format(int(explained_variance * 100)))
-    print(corpus)
+    train_lsa = lsa.fit_transform(tfidf)
+    print(train_lsa)
 
 def buildCorpus(path):
     files = []
@@ -69,6 +118,10 @@ def saveFile(path, filename, text):
 if __name__ == "__main__":
     # corpus from .\completers and .\elicitors
     corpus = buildCorpus(['./suicide-notes-database/completers', './suicide-notes-database/elicitors'])
-    bagOfWords(corpus)
-    print("bow-------------")
+   # bagOfWords(corpus)
     LSA(corpus)
+
+    print("------STATISTIKA-------")
+    statistika = splosnaStatistika('./suicide-notes-database/completers')
+    for i in range(0, len(statistika)):
+        print(statistika[i])
